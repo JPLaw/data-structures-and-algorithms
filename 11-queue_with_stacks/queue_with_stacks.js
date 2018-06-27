4806c7dcc89ee24e849494919e838e8826fe26b0
Jennifer <jlawrence010@gmail.com>
2018-06-27 13:08:41 -0700

    worked on solution, not working yet

---
+++ 11-queue_with_stacks/queue_with_stacks.js
@@ -0,0 +1,20 @@
+let Stack1 = [];
+let Stack2 = [];
+
+function enqStack(element) {
+  Stack1.push(element);
+}
+
+function deqStack() {
+  if (Stack2.length === 0) {
+    if (Stack1.length === 0) { 
+       return 'Cannot dequeue because queue is empty'
+    };
+    while (Stack1.length > 0) {
+      var p = Stack1.pop();
+      Stack2.push(p);
+    }
+  }
+  return Stack2.pop();
+}
+
